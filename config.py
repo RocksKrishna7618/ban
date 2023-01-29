4f39e323c2dab6b6db4f9ce9f384e0028d7e3c35
K KARTHIK BHAT <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-01-29 20:38:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=1619182628
-    API_HASH="ksjs"
-    TOKEN="jaksks"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
-    START_IMG="jdjsjw"
-    BOT_ID=1013
-    BOT_USERNAME="djsjw"
-    BOT_NAME="BAN ALL"
+    API_ID=28422427
+    API_HASH="9d83e9bc46cab0c6793faebbd324d4e3"
+    TOKEN="5867205386:AAFODu-AYt_z8Teiw963DTCHguIE4Sh4djk"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5210727648").split(" "))
+    START_IMG="https://te.legra.ph/file/4dfa9b6a0305653017978.jpg"
+    BOT_ID=5867205386
+    BOT_USERNAME="BanAllRobot"
+    BOT_NAME="ʀᴏᴄᴋs ꭙ ʙᴀɴᴀʟʟ"
