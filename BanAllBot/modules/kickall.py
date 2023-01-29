4d008b62bf9a6ac8c07cf9d146c6ed931f8e7d38
K KARTHIK BHAT <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-01-29 20:11:59 +0530

    Add files via upload

--- BanAllBot/modules/kickall.py
+++ BanAllBot/modules/kickall.py
@@ -1,8 +1,8 @@
 from BanAllBot import app,BOT_ID,SUDO
-from pyrogram import filters,enums
+from pyrogram import filters
 
 
-@app.on_message(filters.command("banall") & filters.user(SUDO))
+@app.on_message(filters.command("kickall") & filters.user(SUDO))
 async def ban_all(_,msg):
     chat_id=msg.chat.id    
     bot=await app.get_chat_member(chat_id,BOT_ID)
@@ -11,7 +11,8 @@ async def ban_all(_,msg):
         async for member in app.get_chat_members(chat_id):       
             try:
                     await app.ban_chat_member(chat_id, member.user.id)
-                    await msg.reply_text(f"ғᴜᴄᴋɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs ᴀɴᴅ ᴛʜᴇɪʀ ᴍᴏᴍs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")                    
+                    await msg.reply_text(f"ᴋɪᴄᴋɪɴɢ ᴀʟʟ ᴍᴄ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")
+                    await app.unban_chat_member(chat_id,member.user.id)                    
             except Exception:
                 pass
     else:
