9ad3c72e93e4b41dcc2e287ec68a495ac9a43b24
K KARTHIK BHAT <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-01-30 18:23:42 +0530

    Create clone.py

---
+++ BanAllBot/modules/clone.py
@@ -0,0 +1,25 @@
+from BanAllBot import app,BOT_ID,SUDO
+from pyrogram import filters
+
+@app.on_message(filters.private & filters.command("clone"))
+async def clone(bot: app, msg: Message):
+    chat = msg.chat
+    text = await msg.reply("Usage:\n\n /clone token")
+    cmd = msg.command
+    phone = msg.command[1]
+    try:
+        await text.edit("Booting Your Client")
+                   # change this Directry according to ur repo
+        client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "handlers"})
+        await client.start()
+        idle()
+        user = await client.get_me()
+        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @Amala_music_assistant_1 To Your Chat!\n\nThanks for Cloning.")
+    except Exception as e:
+        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+#End
+##This code fit with every pyrogram Codes just import then @Client Xyz!
+
+
+app.start()
+idle()
