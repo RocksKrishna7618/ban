134d5f0db4d50e7280718ab252e442515ece2da9
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-19 21:44:59 +0530

    Create clone.py

---
+++ BanAllBot/modules/clone.py
@@ -0,0 +1,14 @@
+from pyrogram import filters, Client
+from config import app, API_ID, API_HASH
+
+@app.on_message(filters.private & filters.command("clone"))
+async def _(app, message):
+     reply = await message.reply("Usage:\n\n/clone <token>")
+     token = message.command[1]
+     try:
+         await reply.edit("Booting Your Client")
+         client = Client (name="Okk", api_id=API_ID, api_hash=API_HASH, bot_token=token, in_memory=True, plugins=dict(root="BanAllBot/plugins"))
+         await client.start()
+         await reply.edit("Your Client Booted Successfully")
+     except Exception as e:
+         await reply.edit("Error:\n`{e}`")
