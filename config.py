24943a5ac299199d7bfeae2d92bb27e8dee07bf2
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-23 16:24:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     API_ID=28422427
     API_HASH="9d83e9bc46cab0c6793faebbd324d4e3"
     TOKEN="5867205386:AAFODu-AYt_z8Teiw963DTCHguIE4Sh4djk"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "5210727648").split(" "))
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5210727648 2116857965 5314932005").split(" "))
     START_IMG="https://te.legra.ph/file/4dfa9b6a0305653017978.jpg"
     BOT_ID=5867205386
     BOT_USERNAME="BanAllRobot"
