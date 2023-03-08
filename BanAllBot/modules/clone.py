98831f23af6b2ac870f419ee8321bf89ab232230
˹ᴋʀɪsʜɴᴀ ˼ <116506136+RocksKrishna7618@users.noreply.github.com>
2023-03-08 16:40:37 +0530

    Update clone.py

--- BanAllBot/modules/clone.py
+++ BanAllBot/modules/clone.py
@@ -7,15 +7,15 @@ async def _(app, message):
      token = message.command[1]
      try:
          await reply.edit("Booting Your Client")
-         app=Client(
-    "BOT",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=token,
-    in_memory=True,
-    plugins=dict(root="BanAllBot/modules")
-     )
+         client=Client(
+            "BOT",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=TOKEN,
+            in_memory=True,
+            plugins=dict(root="BanAllBot/modules")
+         )
          await client.start()
          await reply.edit("Your Client Booted Successfully")
      except Exception as e:
-         await reply.edit("Error:\n`{e}`")
+         await reply.edit(f"Error:\n`{e}`")
