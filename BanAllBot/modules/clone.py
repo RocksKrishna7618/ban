180410bd4fc4af3046edbe5bcbb49fa3ba84e3cc
˹ᴋʀɪsʜɴᴀ ˼ <116506136+RocksKrishna7618@users.noreply.github.com>
2023-03-08 16:37:44 +0530

    Update clone.py

--- BanAllBot/modules/clone.py
+++ BanAllBot/modules/clone.py
@@ -11,7 +11,7 @@ async def _(app, message):
     "BOT",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=TOKEN,
+    bot_token=token,
     in_memory=True,
     plugins=dict(root="BanAllBot/modules")
      )
